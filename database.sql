c0c870a8041491d4def4dd33c573e2d5ff8212e1
tal <talk474747@gmail.com>
2023-08-16 08:25:38 +0300

    backend server should work

---
+++ database.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE users; /*should be for users and also hold squares if I'll have auth system.*/
+
+CREATE TABLE Squares (
+    Squares_amount int
+);
--- server.js
+++ server.js
@@ -6,21 +6,36 @@ const port = 5000
 
 app.use(cors());
 
-app.get('/increment', async (req, res) => {
-  console.log("increment")
-  res.send({ message: 'Increment'});
+app.get('/squares', async (req, res) => {
+  console.log("squares")
   try {
-    const allTodos = await pool.query("SELECT * FROM Persons");
-    console.log(allTodos.rows);
+    const answer = await pool.query("SELECT * FROM Squares");
+    res.send(answer.rows);
   } catch (err) {
     console.error(err.message);
   }
 })
 
-app.get('/reduction', async (req, res) => {
-    console.log("Reduction")
-    res.send({ message: 'Reduction' });
-  })
+app.put('/increment', async (req, res) => {
+  console.log("increment")
+  res.send({ message: 'Increment'});
+  try {
+    await pool.query("UPDATE Squares SET Squares_amount = Squares_amount + 1");
+  } catch (err) {
+    console.error(err.message);
+  }
+})
+
+
+app.put('/reduction', async (req, res) => {
+  console.log("Reduction")
+  res.send({ message: 'Reduction'});
+  try {
+    await pool.query("UPDATE Squares SET Squares_amount = Squares_amount - 1");
+  } catch (err) {
+    console.error(err.message);
+  }
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
